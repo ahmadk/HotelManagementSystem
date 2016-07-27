64020cdb90ffab06ed71f388a6ae9d7ca47bf4e5
arunSenpai <arunSenpai@DESKTOP-0TKF96Q>
2016-07-27 11:55:34 +0300

    Room Class

--- src/HotelManagementSystem/Data/UserDefinedClasses/Room.cs
+++ src/HotelManagementSystem/Data/UserDefinedClasses/Room.cs
@@ -1,1 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
+namespace HotelManagementSystem.Data.UserDefinedClasses
+{
+    public class Room
+    {
+        public string RoomNo { get; set; }
+        public string Floorno { get; set; }
+        public int Capacity { get; set; }
+
+    }
+}
