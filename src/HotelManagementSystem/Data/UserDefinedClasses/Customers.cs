7db6ecbc3847fefb0fc6882b516fb48a08624d52
Tugba <Tugba@DESKTOP-VR3FOIT>
2016-07-27 12:13:27 +0300

    Customer and Employee added

---
+++ src/HotelManagementSystem/Data/UserDefinedClasses/Customers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelManagementSystem.Data.UserDefinedClasses
+{
+    public class Customers
+    {
+        public string name { get; set; }
+        public string lastname { get; set; }
+        public int otherID { get; set; }
+        public string phone { get; set; }
+        public string address { get; set; }
+        public string email { get; set; }
+        public int reservationID { get; set; }
+    }
+}
---
+++ src/HotelManagementSystem/Data/UserDefinedClasses/Employees.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelManagementSystem.Data.UserDefinedClasses
+{
+    public class Employees
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public int employeesID { get; set; }
+
+       
+    }
+}
