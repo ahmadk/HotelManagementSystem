a5342990f047272f56dd3ef68abce66bfba0fb4b
arunSenpai <arunSenpai@DESKTOP-0TKF96Q>
2016-07-27 12:18:11 +0300

    Room Check Aded

---
+++ src/HotelManagementSystem/Data/UserDefinedClasses/RoomCheck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelManagementSystem.Data.UserDefinedClasses
+{
+    public class RoomCheck
+    {
+        public string FK_ReservationId { get; set; }
+        public string Fk_RoomCheckId { get; set; }
+        public string Checkİnfo { get; set; }
+
+
+    }
+}
